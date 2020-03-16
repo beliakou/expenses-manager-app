ef9c7e1e20e942951c0d532e22370d029744d404
Viktar Beliakou <mon-keigh@ya.ru>
2020-03-16 17:16:34 +0300

    add 'find by date between' method to expenses repository

--- expenses-manager-backend/expenses-manager-server/src/main/kotlin/com/github/beliakou/expenses/server/repository/ExpenseRepository.kt
+++ expenses-manager-backend/expenses-manager-server/src/main/kotlin/com/github/beliakou/expenses/server/repository/ExpenseRepository.kt
@@ -2,9 +2,17 @@ package com.github.beliakou.expenses.server.repository
 
 import com.github.beliakou.expenses.server.model.Expense
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDate
 
 @RepositoryRestResource(collectionResourceRel = "expense", path = "expenses")
 interface ExpenseRepository : MongoRepository<Expense, String> {
+
+    fun findAllByUserNameIgnoreCase(name: String): MutableCollection<Expense>
+
+    @Query(value = "{'\$and': [{'user.name' : ?0}, { 'date': { '\$gte': ?1 , '\$lt': ?2 } }] }", sort = "{ 'date': 1 }")
+    fun findAllByDateBetween(name: String, @DateTimeFormat(pattern = "yyyy-MM-dd") from: LocalDate, @DateTimeFormat(pattern = "yyyy-MM-dd") to: LocalDate): MutableCollection<Expense>
 
 }
