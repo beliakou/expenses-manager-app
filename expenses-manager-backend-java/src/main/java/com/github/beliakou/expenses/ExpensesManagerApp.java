6bf3481a70a4c7101d323612be4b37165c910345
Viktar Beliakou <mon-keigh@ya.ru>
2020-04-08 14:18:24 +0300

    add java app

---
+++ expenses-manager-backend-java/src/main/java/com/github/beliakou/expenses/ExpensesManagerApp.java
@@ -0,0 +1,13 @@
+package com.github.beliakou.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesManagerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesManagerApp.class, args);
+	}
+
+}
---
+++ expenses-manager-backend-java/src/test/java/com/github/beliakou/expenses/ExpensesManagerAppTests.java
@@ -0,0 +1,13 @@
+package com.github.beliakou.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesManagerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
